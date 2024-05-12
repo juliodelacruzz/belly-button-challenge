079a7877923245b58d84d7e184a186962cdfc552
juliodelacruzz <julio.dc@icloud.com>
2024-05-12 02:30:01 -0700

    updating app.js

--- Starter_Code/static/js/app.js
+++ Starter_Code/static/js/app.js
@@ -1,16 +1,16 @@
+//link to the json data
 const url = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json'
 
-// Demographic Info
 function panelInfo(id) {
-    d3.json(url).then(function (data) {
+    d3.json(url).then(function (data) { //loading json data from the url specified above
         let sampleData = data;
         let metadata = sampleData.metadata;
-        let identifier = metadata.filter(sample =>
+        let identifier = metadata.filter(sample => //gettign metadata for a sample based on ID
             sample.id.toString() === id)[0];
         let panel = d3.select('#sample-metadata');
         panel.html('');
-        Object.entries(identifier).forEach(([key, value]) => {
-            panel.append('h6').text(`${key}: ${value}`);
+        Object.entries(identifier).forEach(([key, value]) => {  
+            panel.append('h6').text(`${key}: ${value}`); //updating html with ID 
         })
     })
 };
@@ -60,13 +60,13 @@ function Plots(id) {
     })
 };
 
-//Build new upon ID change
+//when selecting a different option in drop down, updating info in plots and panel 
 function optionChanged(id) {
     Plots(id);
     panelInfo(id);
 };
 
-//Test Subject Dropdown and initial function
+//Dropdown basd upon what is selected
 function init() {
     let dropDown = d3.select('#selDataset');
     let id = dropDown.property('value');
